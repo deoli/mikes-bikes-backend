bbb2aa324e24090087f1735c4e27ac07198bd4b8
deoli <deoli@hotmail.com>
2025-01-20 10:33:55 +0000

    Product Part relation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :blueprint
+  has_and_belongs_to_many :parts
 
   # Parent association
   belongs_to :parent,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_17_095021) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_20_102857) do
   create_table "blueprints", force: :cascade do |t|
     t.integer "parent_id"
     t.string "name"
@@ -20,5 +20,32 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_17_095021) do
     t.index ["parent_id"], name: "index_blueprints_on_parent_id"
   end
 
+  create_table "parts", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.integer "stock_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "parts_products", id: false, force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "part_id", null: false
+    t.index ["part_id"], name: "index_parts_products_on_part_id"
+    t.index ["product_id"], name: "index_parts_products_on_product_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "parent_id"
+    t.integer "blueprint_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blueprint_id"], name: "index_products_on_blueprint_id"
+    t.index ["parent_id"], name: "index_products_on_parent_id"
+  end
+
   add_foreign_key "blueprints", "blueprints", column: "parent_id"
+  add_foreign_key "products", "blueprints"
+  add_foreign_key "products", "products", column: "parent_id"
 end
